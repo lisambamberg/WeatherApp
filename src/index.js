61e21c851a7461076ac3d66562b64a2962c72873
Lisa Bamberg <lisambamberg@gmail.com>
2020-02-08 16:47:26 -0600

    Refactored State Initialization

--- src/index.js
+++ src/index.js
@@ -2,20 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: '' };
 
-    this.state = { lat: null, errorMessage: '' };
-
-  }
-
-componentDidMount(){
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-        position => this.setState({ lat: position.coords.latitude }),
-        err => this.setState({ errorMessage: err.message })
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
-}
-
+  }
 
   render() {
     if (this.state.errorMessage && !this.state.lat) {
